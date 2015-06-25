4d4d40345a7b815b818456343dba8e7e50d65255
ccslakey <cromwellslakey@gmail.com>
2015-06-25 16:32:17 -0700

    heroku plz

--- db/migrate/20150623233440_change_pets_columns.rb
+++ db/migrate/20150623233440_change_pets_columns.rb
@@ -1,5 +1,14 @@
-class ChangePetsColumns < ActiveRecord::Migration
-  def change
-  	change_column :pets, :age, :integer
+class ChangePetAgeToInteger < ActiveRecord::Migration
+
+  class << self
+    include AlterColumn
+  end
+  
+  def self.up
+    alter_column :pets, :age, :integer, { "1" => true, "else" => false }, true
+  end
+ 
+  def self.down
+    raise ActiveRecord::IrreversibleMigration.new
   end
 end
