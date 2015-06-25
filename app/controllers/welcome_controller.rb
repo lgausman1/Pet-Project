ca1f16581e5f5b910edb20b30a8266cffaa2cbf9
lgausman1 <l.gausman@comcast.net>
2015-06-25 10:33:11 -0700

    fixed modal in menu, created mockup of about and contact pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,21 @@ class WelcomeController < ApplicationController
 			@user = current_user
 		end
 	end
+
+	def about
+		if current_user
+			@user = current_user
+		end
+
+		render :about
+	end
+
+	def contact
+		if current_user
+			@user = current_user
+		end
+
+		render :contact
+
+	end
 end
