4af73c4fdceaca5d8c6d6e12d6f7c3c08ca38684
rlpacht <rl.pacht@gmail.com>
2015-06-25 15:46:19 -0700

    survey is fixed

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,7 +3,6 @@ class PreferencesController < ApplicationController
 	def create
 		@preference = Preference.create(survey_params)
 		user_id = current_user.id
-		binding.pry
 		redirect_to "/users/#{user_id}/matches"
 	end
 
@@ -15,10 +14,12 @@ class PreferencesController < ApplicationController
 
 	def edit
 		@user = User.find(current_user)
+		@preference = Preference.find_by({user_id: params[:id]})
 		render :edit
 	end
 
 	def update
+		@preference = Preference.find_by({user_id: current_user.id})
 		@preference.update(survey_params)
 		redirect_to "/users/#{@current_user.id}/matches"
 	end
