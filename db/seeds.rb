ccfa7b328de2fb45192a43ebdbab03088d66cd6e
lgausman1 <l.gausman@comcast.net>
2015-08-01 16:50:36 -0700

    removed picture from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,7 @@ pets = Pet.create([
 		age:"4M",
 		weight:"4lbs. 6oz.",
 		gender:"Male",
-		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28240948-2fab58f0.jpg?itok=jCnn3pXU",
-			text:""
-			}
+		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!"
 	},
 	{
 		name:"Archer",
@@ -18,12 +13,7 @@ pets = Pet.create([
 		age:"1Y 0M",
 		weight:"8lbs. 13oz.",
 		gender:"Male",
-		description:"If you hunger for a best friend, come meet Archer. Archer is a cute petite orange tabby with a white bib and white socks. He will greet you at the door and then aim right for your lap. Give him a few head and jaw scratches and he will reward you with a nose to nose rub. Archer loves to play as much as he loves to cuddle. He will even tell you a few stories in his sing-song trilling meow. You will hit the bulls eye with Archer at your side. Come meet this sweet affectionate tabby today. You may be the perfect match for each other.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/27968642-17de4a58.jpg?itok=QtyCIwe6",
-			text:""
-			}
+		description:"If you hunger for a best friend, come meet Archer. Archer is a cute petite orange tabby with a white bib and white socks. He will greet you at the door and then aim right for your lap. Give him a few head and jaw scratches and he will reward you with a nose to nose rub. Archer loves to play as much as he loves to cuddle. He will even tell you a few stories in his sing-song trilling meow. You will hit the bulls eye with Archer at your side. Come meet this sweet affectionate tabby today. You may be the perfect match for each other."
 	},
 	{
 		name:"Virginia",
@@ -31,12 +21,7 @@ pets = Pet.create([
 		age:"2M",
 		weight:"2lbs. 3oz.",
 		gender:"Female",
-		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28628829-photo.jpg?itok=N2ewn5Nn",
-			text:""
-			}
+		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!"
 	},
 	{
 		name:"Permelia",
@@ -45,11 +30,6 @@ pets = Pet.create([
 		weight:"2lbs. 3oz.",
 		gender:"Female",
 		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28628160-3423478b.jpg?itok=jG7WWu4o",
-			text:""
-			}
 	},
 	{
 		name:"Mark",
@@ -57,12 +37,7 @@ pets = Pet.create([
 		age:"3M",
 		weight:"4lbs. 6oz.",
 		gender:"Male",
-		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28628052-79ee8af2.jpg?itok=EYarZUdo",
-			text:""
-			}
+		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!"
 	},
 	{
 		name:"Maranella",
@@ -70,12 +45,7 @@ pets = Pet.create([
 		age:"3M",
 		weight:"2lbs. 3oz.",
 		gender:"Female",
-		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28628064-a80bca7d.jpg?itok=YGYRY_6Z",
-			text:""
-			}
+		description:"I'm a curious kitten looking for adopters who can commit to continuing my education and socialization. I am ready for a home with snuggles and playtime and lots of kitten-love! I am looking forward to some gentle handling and daily play sessions with you. The SF SPCA offers a free Kitten Adoption Handbook so we will get off to a great start!"
 	},
 	{
 		name:"Juju Bean",
@@ -84,12 +54,7 @@ pets = Pet.create([
 		weight:"6lbs. 10oz.",
 		gender:"Female",
 		personality:"Lion Hearted",
-		description:"You see that I am a brown tabby. You know what that means? That I am really smart! If you need help with a math problem, I'll be there. Can't spell pterodactyl, just ask me. Want help with your taxes, I'm your gal. I even know how to walk on a leash! I am a chatty girl who is more than happy to tell you what is on my mind. I love to cuddle and will choose that over playtime, if given a choice. I haven't let them hear my purr yet, I think I will save that just for you. Drop by and meet me. We can discuss how much you need me around and the new sleeping arrangements. I hope to see you soon!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/24197498-738aff1c.jpg?itok=RtVPLVfG",
-			text:""
-			}
+		description:"You see that I am a brown tabby. You know what that means? That I am really smart! If you need help with a math problem, I'll be there. Can't spell pterodactyl, just ask me. Want help with your taxes, I'm your gal. I even know how to walk on a leash! I am a chatty girl who is more than happy to tell you what is on my mind. I love to cuddle and will choose that over playtime, if given a choice. I haven't let them hear my purr yet, I think I will save that just for you. Drop by and meet me. We can discuss how much you need me around and the new sleeping arrangements. I hope to see you soon!"
 	},
 	{
 		name:"Wampus",
@@ -98,12 +63,7 @@ pets = Pet.create([
 		weight:"15lbs. 6oz.",
 		gender:"Male",
 		personality:"Cat Next Door i",
-		description:"Hi, y'all - I'm Wampus! Now I know you must be asking yourself, what's a Wampus? Why, it's a cat like me! Big, tough, fierce, legendary - but underneath it all, a fluffy pussycat. Because I'm black and white, you can also call me a Jellicle cat. As you can see, I'm pretty special, but hey, I'm easy to please. Chin scratches, jaw rubs - yeah, keep them coming. That rumbling sound you'll hear? That's me, purring, in my most contented Jellicle/Wampus way. Why don't you come out today and meet me?",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/25140783-21b997a8.jpg?itok=OXBNuahn",
-			text:""
-			}
+		description:"Hi, y'all - I'm Wampus! Now I know you must be asking yourself, what's a Wampus? Why, it's a cat like me! Big, tough, fierce, legendary - but underneath it all, a fluffy pussycat. Because I'm black and white, you can also call me a Jellicle cat. As you can see, I'm pretty special, but hey, I'm easy to please. Chin scratches, jaw rubs - yeah, keep them coming. That rumbling sound you'll hear? That's me, purring, in my most contented Jellicle/Wampus way. Why don't you come out today and meet me?"
 	},
 	{
 		name:"Rowdy",
@@ -114,12 +74,7 @@ pets = Pet.create([
 		breed:"Chihuahua, Short Coat i",
 		activity_level:"High",
 		personality:"Busy Bee i",
-		description:"Rowdy is true to his namesake - this boy is a party animal! He loves to play, run, jump, climb, play some more, and maybe even get into a little bit of mischief. Rowdy will benefit from basic training classes to help him focus his energy, bond with his adopters, and learn how to be a well-mannered companion. He would like to join an adult-only home or a family with children over the age of 10.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28670514-b56ce4f2.jpg?itok=w1qIDTZp",
-			text:""
-			}
+		description:"Rowdy is true to his namesake - this boy is a party animal! He loves to play, run, jump, climb, play some more, and maybe even get into a little bit of mischief. Rowdy will benefit from basic training classes to help him focus his energy, bond with his adopters, and learn how to be a well-mannered companion. He would like to join an adult-only home or a family with children over the age of 10."
 	},
 	{
 		name:"Feather",
@@ -130,12 +85,7 @@ pets = Pet.create([
 		breed:"Chihuahua, Short Coat i",
 		activity_level:"Medium",
 		personality:"Delicate Flower i",
-		description:"Feather is a nice pooch that came from the local shelter. This sensitive, sweet gal needs some adopters that can help her to build up more confidence through praise, encouragement and continuous positive experiences. She will do well with caring adopters that can give her daily exercise, some training, chances to mingle with other dogs of like size and lots of affection. Feather may fare better in a quieter residence without toddlers present.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28658098-b88f69fd.jpg?itok=EJE-hYIH",
-			text:""
-			}
+		description:"Feather is a nice pooch that came from the local shelter. This sensitive, sweet gal needs some adopters that can help her to build up more confidence through praise, encouragement and continuous positive experiences. She will do well with caring adopters that can give her daily exercise, some training, chances to mingle with other dogs of like size and lots of affection. Feather may fare better in a quieter residence without toddlers present."
 	},
 	{
 		name:"Bo",
@@ -144,12 +94,7 @@ pets = Pet.create([
 		weight:"66lbs. 2oz.",
 		gender:"Male",
 		breed:"Terrier, Pit Bull i",
-		description:"Beautiful blue eyed Bo is looking into your soul to find his true love! Bo would like an adult only home with active, caring adopter who can give him plenty of affection, exercise, training and care. He also enjoys playing with toys, especially tennis balls, and chances to play with other dogs. It is important for a dog as Bo to remain very socialized to humans and dogs alike. Bo needs an adopter that will get off on the right foot with him, giving him continual praise, positive experiences and chances to interact with the world around him.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/25219825-404c74c5.jpg?itok=6AfwGX5L",
-			text:""
-			}
+		description:"Beautiful blue eyed Bo is looking into your soul to find his true love! Bo would like an adult only home with active, caring adopter who can give him plenty of affection, exercise, training and care. He also enjoys playing with toys, especially tennis balls, and chances to play with other dogs. It is important for a dog as Bo to remain very socialized to humans and dogs alike. Bo needs an adopter that will get off on the right foot with him, giving him continual praise, positive experiences and chances to interact with the world around him."
 	},
 	{
 		name:"Tatum",
@@ -158,12 +103,7 @@ pets = Pet.create([
 		weight:"24lbs. 5oz.",
 		gender:"Male",
 		breed:"Terrier i",
-		description:"Tatum is a great mid-size dog. He loves his walks and is very social with both people and other dogs. If you are in the market for an easy going and happy new companion than Tatum would love to meet you.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28634485-13b4344c.jpg?itok=3XbXnYPD",
-			text:""
-			}
+		description:"Tatum is a great mid-size dog. He loves his walks and is very social with both people and other dogs. If you are in the market for an easy going and happy new companion than Tatum would love to meet you."
 	},
 	{
 		name:"Della",
@@ -172,12 +112,7 @@ pets = Pet.create([
 		weight:"81lbs. 10oz.",
 		gender:"Female",
 		breed:"Retriever, Labrador i",
-		description:"Lovely Della came from San Joaquin County. She is sweet as can be and ready to go home with active, caring adopters that can offer plenty of affection, daily exercise, and some training. She will benefit from some chances to mingle with other dogs her size so she can have fun playing and to maintain a healthy sociability with her fellow canines.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/28609426-bf9adbf0.jpg?itok=zmvblZkT",
-			text:""
-			}
+		description:"Lovely Della came from San Joaquin County. She is sweet as can be and ready to go home with active, caring adopters that can offer plenty of affection, daily exercise, and some training. She will benefit from some chances to mingle with other dogs her size so she can have fun playing and to maintain a healthy sociability with her fellow canines."
 	},
 	{
 		name:"Barley",
@@ -186,12 +121,7 @@ pets = Pet.create([
 		weight:"6lbs. 10oz.",
 		gender:"Female",
 		breed:"Australian Shepherd i",
-		description:"Barley is a sensitive girl who knows exactly what she wants in life. All she wants is a place to call all her own with older people surrounding her with love! She enjoys being in the spotlight, so she insists on being the only dog in the home. She would benefit from reward-based training classes along with lots of physical exercise to help boost her confidence.",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/14286251-8cc695e3.jpg?itok=3eaTpX4z",
-			text:""
-			}
+		description:"Barley is a sensitive girl who knows exactly what she wants in life. All she wants is a place to call all her own with older people surrounding her with love! She enjoys being in the spotlight, so she insists on being the only dog in the home. She would benefit from reward-based training classes along with lots of physical exercise to help boost her confidence."
 	},
 	{
 		name:"Lana",
@@ -200,12 +130,7 @@ pets = Pet.create([
 		weight:"11lbs.",
 		gender:"Female",
 		breed:"Chihuahua, Short Coat i",
-		description:"Gentle Lana is looking for a quiet peaceful place with a doggy sibling to call her own. She may be a bit timid at first, but once she gets to know you she will be your BFF! Lana enjoys going on lots of walks and playing with other size-like dogs. Nala would benefit for reward based training and some confidence building. Come meet this delicate flower today!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/23683847-95ac59ae.jpg?itok=Urg3mPpz",
-			text:""
-			}
+		description:"Gentle Lana is looking for a quiet peaceful place with a doggy sibling to call her own. She may be a bit timid at first, but once she gets to know you she will be your BFF! Lana enjoys going on lots of walks and playing with other size-like dogs. Nala would benefit for reward based training and some confidence building. Come meet this delicate flower today!"
 	},
 	{
 		name:"Charley",
@@ -214,12 +139,7 @@ pets = Pet.create([
 		weight:"57lbs. 5oz.",
 		gender:"Male",
 		breed:"Retriever, Labrador i",
-		description:"Charley is a big boy with a goofy smile! He has lots of energy and eyes for only you so he requires you to stay close! Charley would love some more training classes to keep his mind in tip top shape! He enjoys cuddling next to you, but really would love to go to the park & play to his heart's content! Come meet this goofy guy today!",
-		picture:{
-			alt:"",
-			src:"https://www.sfspca.org/sites/default/files/styles/480_width/public/images/animals/20818764-2965006d.jpg?itok=cSdaINaX",
-			text:""
-			}
+		description:"Charley is a big boy with a goofy smile! He has lots of energy and eyes for only you so he requires you to stay close! Charley would love some more training classes to keep his mind in tip top shape! He enjoys cuddling next to you, but really would love to go to the park & play to his heart's content! Come meet this goofy guy today!"
 	}
 ])
 
