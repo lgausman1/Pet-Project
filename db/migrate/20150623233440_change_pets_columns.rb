17de7ec22c7868596bec95dfda28eea26f9592ed
lgausman1 <l.gausman@comcast.net>
2015-08-01 16:33:56 -0700

    modified migrations to convert age, weight to integer

--- db/migrate/20150623233440_change_pets_columns.rb
+++ db/migrate/20150623233440_change_pets_columns.rb
@@ -1,5 +1,6 @@
 class ChangePetsColumns < ActiveRecord::Migration
   def change
-  	change_column :pets, :age, :integer
+  	#change_column :pets, :age, :integer
+  	change_column :pets, :age, 'integer USING CAST(age AS integer)'
   end
 end
--- db/migrate/20150624032848_change_pet_weight_to_integer.rb
+++ db/migrate/20150624032848_change_pet_weight_to_integer.rb
@@ -1,5 +1,6 @@
 class ChangePetWeightToInteger < ActiveRecord::Migration
   def change
-  	change_column :pets, :weight, :integer
+  	#change_column :pets, :weight, :integer
+  	change_column :pets, :weight, 'integer USING CAST(weight AS integer)'
   end
 end
