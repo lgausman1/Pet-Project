3a746da9b948b8f26504667424463fc546c988ed
lgausman1 <l.gausman@comcast.net>
2015-08-06 17:01:08 -0700

    added dependent: :delete to user_pet on pet.rb

--- app/models/user_pet.rb
+++ app/models/user_pet.rb
@@ -1,6 +1,6 @@
 class UserPet < ActiveRecord::Base
   belongs_to :user
   # added dependent: :delete
-  belongs_to :pet, dependent: :delete
+  belongs_to :pet
 end
 # update or delete on table "pets" violates foreign key constraint "fk_rails_dedd5edecd" on table "user_pets
